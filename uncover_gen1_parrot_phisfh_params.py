0f5e8d392011d7c9e20f51ffa1f10b59b8f304e3
ikkimitsuhashi <ikki0913astr@gmail.com>
2025-07-08 00:00:24 -0600

    edit parrot.py to test with v002

--- uncover_gen1_parrot_phisfh_params.py
+++ uncover_gen1_parrot_phisfh_params.py
@@ -165,7 +165,6 @@ badobs_ids_list = []
 for ifit in np.arange(args.idx0, args.idx1, 1):
 
     # run on the full catalog
-    print(cat['id'])
     objid = cat['id'][ifit]
     print("\nFitting {}".format(objid))
     print("------------------\n")
