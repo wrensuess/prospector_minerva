a24ab6c878a7e0333554329221741976d107ff2f
ikkimitsuhashi <ikki0913astr@gmail.com>
2025-08-31 09:57:39 +0900

    change submit.py and use unfit catalog

--- submit.py
+++ submit.py
@@ -95,7 +95,8 @@ if __name__ == '__main__':
     catalog = 'MINERVA-{}_{}_CATALOG.fits'.format(field, ver)
     #fitcatalog = catdir+'fitid_MINERVA-{}_{}_CATALOG.txt'.format(field, ver)
     #fitcatalog = catdir+'unfitid_MINERVA-{}_{}_CATALOG.txt'.format(field, ver)
-    fitcatalog = catdir+'unfitid2_MINERVA-{}_{}_CATALOG.txt'.format(field, ver) #file includes use_photo=1 sources
+    #fitcatalog = catdir+'unfitid2_MINERVA-{}_{}_CATALOG.txt'.format(field, ver)
+    fitcatalog = catdir+'unfitid3_MINERVA-{}_{}_CATALOG.txt'.format(field, ver) #file includes use_photo=1 sources
     ids_fit = np.loadtxt(fitcatalog)
     print('total number of galaxies to fit:',len(ids_fit)) #[Nfit]
 
