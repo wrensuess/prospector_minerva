6562d654f00386eae4b80429dc4d35b049a4a874
ikkimitsuhashi <ikki0913astr@gmail.com>
2025-12-01 17:12:39 -0700

    edit chain.py to check loop

--- chain_jobs.py
+++ chain_jobs.py
@@ -77,16 +77,16 @@ def get_unfit_number(catdir,cathead,field,ver):
     return int(len(not_fitted)), np.array(not_fitted_out)
 
     
-'''
+
 field = "UDS"
 ver = "n2.3_v1.1"
 catdir = '../phot_catalog/'
 cathead = "fitid"
 catpath = catdir+cathead+"_MINERVA-"+field+"_"+ver+"_LW_Kf444w_SUPER_CATALOG.txt"
-subprocess.run(["python", "submit_loop.py", cathead, field, ver], check=True)
-time.sleep(CHECK_INTERVAL_SEC)
+#subprocess.run(["python", "submit_loop.py", cathead, field, ver], check=True)
+#time.sleep(CHECK_INTERVAL_SEC)
 
-k=0
+k=4#0
 while True:
     wait_job_finish()
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -99,12 +99,12 @@ while True:
         cathead = "unfitid"+str(int(k))
         catpath = catdir+cathead+"_MINERVA-"+field+"_"+ver+"_LW_Kf444w_SUPER_CATALOG.txt"
         np.savetxt(catpath,np.array(unfit_id_array))
-        subprocess.run(["python", "submit_loop.py", cathead, field, ver], check=True)
-        print(f"[INFO] job with "+cathead+" catalog is running...")
-        time.sleep(CHECK_INTERVAL_SEC)
+        #subprocess.run(["python", "submit_loop.py", cathead, field, ver], check=True)
+        #print(f"[INFO] job with "+cathead+" catalog is running...")
+        #time.sleep(CHECK_INTERVAL_SEC)
+        print(dfafa)
 
 print(f"[INFO] all fitting is completed")
-'''
 
 field = "UDS"
 ver = "n2.3_v1.1"
