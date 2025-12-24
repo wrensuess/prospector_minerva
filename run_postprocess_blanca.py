c6889e06492ffd75baadec7137ea8728f0faa695
ikkimitsuhashi <ikki0913astr@gmail.com>
2025-12-24 14:50:45 -0700

    Edit path for postprocess

--- run_postprocess_blanca.py
+++ run_postprocess_blanca.py
@@ -43,16 +43,14 @@ if last==0:
     ids_fit = ids[np.where(np.isin(ids, ids_comp)==False)[0]]
     ids = ids_fit
 print(len(ids))
-print(ids)
 
 ids_split = np.array_split(ids, njobs)
 # save each chunk to a separate file
 os.makedirs(outdir+'id_files', exist_ok=True)
 for i, ids_chunk in enumerate(ids_split):
     np.savetxt(f'{outdir}/id_files/ids_postprocess_{i}.txt', ids_chunk, fmt='%d')
-    print(i)
 
-'''
+
 # now create the command to submit
 _cmd = "python -u postprocess_parrot_wrap.py --prior {} --fit 'fid' --catalog {} --indir {} --outdir {} --narr {} --iarr {} --ids_file {} --ddir {}".format(prior, 
     catalog, indir, outdir+'npz/', narr, iarr, outdir+'id_files/ids_postprocess_${SLURM_ARRAY_TASK_ID}.txt', ddir)
@@ -87,4 +85,3 @@ with open(outdir+'slurm_postprocess.sh', 'w') as f:
     f.write(txt_acc)
 
 os.system('sbatch {}/slurm_postprocess.sh'.format(outdir))    
-'''
