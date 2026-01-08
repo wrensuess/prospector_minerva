733d96a71c5a988c05fb0952954ab0a2b12e4e6d
ikkimitsuhashi <ikki0913astr@gmail.com>
2026-01-07 17:50:06 -0700

    change catalog to postprocess

--- run_postprocess_blanca.py
+++ run_postprocess_blanca.py
@@ -36,51 +36,85 @@ ts = time.strftime("%y%b%d-%H.%M", time.localtime())
 ids = np.array([int(i.split('_')[1]) for i in  os.listdir(indir)])
 
 last = 0
-if last==0:
+
+if last!=0:
+    ids_split = np.array_split(ids, njobs)
+    # save each chunk to a separate file
+    os.makedirs(outdir+'id_files', exist_ok=True)
+    for i, ids_chunk in enumerate(ids_split):
+        np.savetxt(f'{outdir}/id_files/ids_postprocess_{i}.txt', ids_chunk, fmt='%d')
+
+    # now create the command to submit
+    _cmd = "python -u postprocess_parrot_wrap.py --prior {} --fit 'fid' --catalog {} --indir {} --outdir {} --narr {} --iarr {} --ids_file {} --ddir {}".format(prior, 
+        catalog, indir, outdir+'npz/', narr, iarr, outdir+'id_files/ids_postprocess_${SLURM_ARRAY_TASK_ID}.txt', ddir)
+
+    # and make our slurm file 
+    txt_acc = '\n'.join(["#!/bin/bash -l",
+                                 "#SBATCH --account=blanca-casa\n",
+                                 "#SBATCH --partition=blanca\n",
+                                 "#SBATCH --qos=preemptable\n",
+                                 "#SBATCH --time={:d}:00:00\n".format(wtime),
+                                 "#SBATCH --nodes=1",
+                                 "#SBATCH --ntasks=1",
+                                 "#SBATCH --job-name={}".format(jname),
+                                 "#SBATCH --array=0-{}".format(int(njobs-1)),
+                                 "#SBATCH --output={}/{}_{}_%A_%a.out".format(log_dir, jname, ts),
+                                 "#SBATCH --error={}/{}_{}_%A_%a.err".format(log_dir, jname, ts),
+                                 "",
+                                 'now=$(date +"%T")',
+                                 'echo "start time ... $now"',
+                                 'echo "Running task ID: ${SLURM_ARRAY_TASK_ID}"',
+                                 "",
+                                 'module purge',
+                                 'module load anaconda',
+                                 "source activate {}".format(env),
+                                 "",
+                                 "cd {}".format(code_dir),
+                                 _cmd,
+                                 'now=$(date +"%T")',
+                                 'echo "end time ... $now"',
+                                 ""])
+
+if last==0: 
     # this is added to adjust ids, for the last ~a few % of the sources
     ids_comp_ = glob.glob(outdir+"npz/*_spec_phisfh.npz")
     ids_comp = [float(a.split("/")[-1].split("_")[1]) for a in ids_comp_]
     ids_fit = ids[np.where(np.isin(ids, ids_comp)==False)[0]]
     ids = ids_fit
-print(len(ids))
+    print(len(ids))
+    np.savetxt(f'{outdir}/id_files/ids_postprocess_0.txt', ids, fmt='%d')
+    
+    _cmd = "python -u postprocess_parrot_wrap.py --prior {} --fit 'fid' --catalog {} --indir {} --outdir {} --narr {} --iarr {} --ids_file {} --ddir {}".format(prior, 
+        catalog, indir, outdir+'npz/', narr, iarr, outdir+'id_files/ids_postprocess_0.txt', ddir)
 
-ids_split = np.array_split(ids, njobs)
-# save each chunk to a separate file
-os.makedirs(outdir+'id_files', exist_ok=True)
-for i, ids_chunk in enumerate(ids_split):
-    np.savetxt(f'{outdir}/id_files/ids_postprocess_{i}.txt', ids_chunk, fmt='%d')
+    # and make our slurm file 
+    txt_acc = '\n'.join(["#!/bin/bash -l",
+                                 "#SBATCH --account=blanca-casa\n",
+                                 "#SBATCH --partition=blanca\n",
+                                 "#SBATCH --qos=preemptable\n",
+                                 "#SBATCH --time={:d}:00:00\n".format(wtime),
+                                 "#SBATCH --nodes=1",
+                                 "#SBATCH --ntasks=1",
+                                 "#SBATCH --job-name={}".format(jname),
+                                 "#SBATCH --array=0",
+                                 "#SBATCH --output={}/{}_{}_%A_%a.out".format(log_dir, jname, ts),
+                                 "#SBATCH --error={}/{}_{}_%A_%a.err".format(log_dir, jname, ts),
+                                 "",
+                                 'now=$(date +"%T")',
+                                 'echo "start time ... $now"',
+                                 'echo "Running task ID: 0"',
+                                 "",
+                                 'module purge',
+                                 'module load anaconda',
+                                 "source activate {}".format(env),
+                                 "",
+                                 "cd {}".format(code_dir),
+                                 _cmd,
+                                 'now=$(date +"%T")',
+                                 'echo "end time ... $now"',
+                                 ""])
+    
 
-
-# now create the command to submit
-_cmd = "python -u postprocess_parrot_wrap.py --prior {} --fit 'fid' --catalog {} --indir {} --outdir {} --narr {} --iarr {} --ids_file {} --ddir {}".format(prior, 
-    catalog, indir, outdir+'npz/', narr, iarr, outdir+'id_files/ids_postprocess_${SLURM_ARRAY_TASK_ID}.txt', ddir)
-
-# and make our slurm file 
-txt_acc = '\n'.join(["#!/bin/bash -l",
-                             "#SBATCH --account=blanca-casa\n",
-                             "#SBATCH --partition=blanca\n",
-                             "#SBATCH --qos=preemptable\n",
-                             "#SBATCH --time={:d}:00:00\n".format(wtime),
-                             "#SBATCH --nodes=1",
-                             "#SBATCH --ntasks=1",
-                             "#SBATCH --job-name={}".format(jname),
-                             "#SBATCH --array=0-{}".format(int(njobs-1)),
-                             "#SBATCH --output={}/{}_{}_%A_%a.out".format(log_dir, jname, ts),
-                             "#SBATCH --error={}/{}_{}_%A_%a.err".format(log_dir, jname, ts),
-                             "",
-                             'now=$(date +"%T")',
-                             'echo "start time ... $now"',
-                             'echo "Running task ID: ${SLURM_ARRAY_TASK_ID}"',
-                             "",
-                             'module purge',
-                             'module load anaconda',
-                             "source activate {}".format(env),
-                             "",
-                             "cd {}".format(code_dir),
-                             _cmd,
-                             'now=$(date +"%T")',
-                             'echo "end time ... $now"',
-                             ""])
 with open(outdir+'slurm_postprocess.sh', 'w') as f:
     f.write(txt_acc)
 
