7015a016e0bbf7e3d8428154f0ac7fb453a49ba8
ikkimitsuhashi <ikki0913astr@gmail.com>
2026-01-09 01:35:39 -0700

    change catalog to postprocess

--- run_postprocess_blanca.py
+++ run_postprocess_blanca.py
@@ -82,9 +82,6 @@ if last==0:
     ids_fit = ids[np.where(np.isin(ids, ids_comp)==False)[0]]
     ids = ids_fit
     print(len(ids))
-    print(ids)
-    print(ids[1:])
-    print(dfaf)
     np.savetxt(f'{outdir}/id_files/ids_postprocess_0.txt', ids[1:], fmt='%d')
     
     _cmd = "python -u postprocess_parrot_wrap.py --prior {} --fit 'fid' --catalog {} --indir {} --outdir {} --narr {} --iarr {} --ids_file {} --ddir {}".format(prior, 
