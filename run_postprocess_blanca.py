309c211ca4cf5e0540722e4e36a38e892a82a6f8
ikkimitsuhashi <ikki0913astr@gmail.com>
2026-04-10 15:00:24 -0600

    test savez_compressed

--- run_postprocess_blanca.py
+++ run_postprocess_blanca.py
@@ -36,6 +36,7 @@ ts = time.strftime("%y%b%d-%H.%M", time.localtime())
 
 # first let's split up the finished IDs into njobs parts
 ids = np.array([int(i.split('_')[1]) for i in os.listdir(indir)])
+ids = ids[:,200]
 print(len(ids))
 print(dfafa)
 
