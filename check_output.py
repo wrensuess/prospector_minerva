70d2bd1a07f86824700d06fc1689e7df18b693d4
ikkimitsuhashi <ikki0913astr@gmail.com>
2026-04-15 10:06:15 -0600

    test h5py read

--- check_output.py
+++ check_output.py
@@ -208,5 +208,5 @@ fsed = np.load(ffsed, allow_pickle=True)
 fsfh = np.load(ffsfh, allow_pickle=True)
 fchain = np.load(ffchain, allow_pickle=True)
 '''
-print(z_phot[np.where(id_mega_photo==1109763)])
-print(z_phot[np.where(id_mega_photo==1109764)])
+print(z_phot[np.where(id_mega_photo==1235366)])
+print(z_phot[np.where(id_mega_photo==1235367)])
