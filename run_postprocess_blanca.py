b2bfb0fdf2b8634d429409cdd9cbf73e30676361
ikkimitsuhashi <ikki0913astr@gmail.com>
2026-04-29 09:56:43 -0600

    re-run broken spec files

--- run_postprocess_blanca.py
+++ run_postprocess_blanca.py
@@ -42,9 +42,9 @@ ids = np.array([int(i.split('_')[1]) for i in os.listdir(indir)])
 ids = np.loadtxt(outdir+"ids_broken_spec.txt")
 print(ids)
 
-last = 0#0
+process = 2#0
 
-if last!=0:
+if process==0:
     ids_split = np.array_split(ids, njobs)
     # save each chunk to a separate file
     os.makedirs(outdir+'id_files', exist_ok=True)
@@ -81,12 +81,47 @@ if last!=0:
                                  'echo "end time ... $now"',
                                  ""])
 
-if last==0: 
+if process==1:
     # this is added to adjust ids, for the last ~a few % of the sources
     ids_comp_ = glob.glob(outdir+"npz/*_spec_phisfh.h5")
     ids_comp = [float(a.split("/")[-1].split("_")[1]) for a in ids_comp_]
     ids_fit = ids[np.where(np.isin(ids, ids_comp)==False)[0]]
     ids = ids_fit
+    print(len(ids))
+    np.savetxt(f'{outdir}/id_files/ids_postprocess_9999.txt', ids, fmt='%d')
+    
+    _cmd = "python -u postprocess_parrot_wrap.py --prior {} --fit 'fid' --catalog {} --indir {} --outdir {} --narr {} --iarr {} --ids_file {} --ddir {} --faildir {}".format(prior, catalog, indir, outdir+'npz/', narr, iarr, outdir+'id_files/ids_postprocess_9999.txt', ddir, outdir+'fail/')
+
+    # and make our slurm file 
+    txt_acc = '\n'.join(["#!/bin/bash -l",
+                                 "#SBATCH --account=blanca-casa\n",
+                                 "#SBATCH --partition=blanca\n",
+                                 "#SBATCH --qos=preemptable\n",
+                                 "#SBATCH --time={:d}:00:00\n".format(wtime),
+                                 "#SBATCH --nodes=1",
+                                 "#SBATCH --ntasks=1",
+                                 "#SBATCH --job-name={}".format(jname),
+                                 "#SBATCH --array=0",
+                                 "#SBATCH --output={}/{}_{}_%A_%a.out".format(log_dir, jname, ts),
+                                 "#SBATCH --error={}/{}_{}_%A_%a.err".format(log_dir, jname, ts),
+                                 "",
+                                 'now=$(date +"%T")',
+                                 'echo "start time ... $now"',
+                                 'echo "Running task ID: 0"',
+                                 "",
+                                 'module purge',
+                                 'module load anaconda',
+                                 "source activate {}".format(env),
+                                 "",
+                                 "cd {}".format(code_dir),
+                                 _cmd,
+                                 'now=$(date +"%T")',
+                                 'echo "end time ... $now"',
+                                 ""])
+
+if process==2:
+    # this is added to re-run several sources, which is broken in either spec or perc. typically ~a few % of the sources
+    ids = ids
     print(len(ids))
     np.savetxt(f'{outdir}/id_files/ids_postprocess_9999.txt', ids, fmt='%d')
     
