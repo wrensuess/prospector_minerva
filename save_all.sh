5f267b5cbf2b9bf862ed33d1d8184b09ac848376
ikkimitsuhashi <ikki0913astr@gmail.com>
2026-05-11 10:55:17 -0600

    re-run broken spec files

--- save_all.sh
+++ save_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=run_chan_job
-#SBATCH --partition=blanca-casa
-#SBATCH --qos=blanca-casa
+#SBATCH --partition=amilan
+#SBATCH --qos=normal
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
