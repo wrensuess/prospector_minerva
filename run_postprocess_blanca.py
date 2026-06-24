3a240e1d3d5f6ca3cd3a22ec000bc2914fd480f0
ikkimitsuhashi <ikki0913astr@gmail.com>
2026-06-23 22:11:41 -0600

    postprocess nested_target_n_effective 20000

--- run_postprocess_blanca.py
+++ run_postprocess_blanca.py
@@ -13,9 +13,9 @@ from utils import get_dir
 #log_dir = '/scratch/alpine/kasu8993/postprocess_n2.2_m1.0_v1.0/logs/'
 
 catalog = '/projects/ikmi3774/minerva_sps_git/stellar_pop_catalog_bb/phot_catalog/MINERVA-UDS_n3.0_v1.2_ACS+WEBB_Kf444w_SUPER_CATALOG.fits'
-indir = '/scratch/alpine/ikmi3774/slurm/chains_parrot_UDS_n3.0_v1.2_ACS+WEBB_Kf444w_SUPER_spsbeta/'
-outdir = '/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2/'
-log_dir = '/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2/logs/'
+indir = '/scratch/alpine/ikmi3774/slurm/chains_parrot_UDS_n3.0_v1.2_ACS+WEBB_Kf444w_SUPER_spsbeta_massive20000/'
+outdir = '/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2_massive20000/'
+log_dir = '/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2_massive20000/logs/'
 #outdir = '/scratch/alpine1/kasu8993/postprocess_UDS_n3.0_v1.2/'
 #log_dir = '/scratch/alpine1/kasu8993/postprocess_UDS_n3.0_v1.2/logs/'
 
@@ -40,12 +40,52 @@ ids = np.array([int(i.split('_')[1]) for i in os.listdir(indir)])
 
 # need to re-run for broken file, process=2
 # code is implemented to *DELETE* these ids' postprocess files once, because default postprocess code skip the existing ids
-ids = np.loadtxt(outdir+"ids_broken_spec.txt")
+#ids = np.loadtxt(outdir+"ids_broken_spec.txt")
+
+print(len(ids))
 print(ids)
 
-process = 2#0
+process = -1
 
-if process==0:
+if process==-1: ### priority node
+    njobs = 100
+    ids_split = np.array_split(ids, njobs)
+    # save each chunk to a separate file
+    os.makedirs(outdir+'id_files', exist_ok=True)
+    for i, ids_chunk in enumerate(ids_split):
+        np.savetxt(f'{outdir}/id_files/ids_postprocess_{i}.txt', ids_chunk, fmt='%d')
+
+    # now create the command to submit
+    _cmd = "python -u postprocess_parrot_wrap.py --prior {} --fit 'fid' --catalog {} --indir {} --outdir {} --narr {} --iarr {} --ids_file {} --ddir {} --faildir {}".format(prior, catalog, indir, outdir+'npz/', narr, iarr, outdir+'id_files/ids_postprocess_${SLURM_ARRAY_TASK_ID}.txt', ddir, outdir+'fail/')
+
+    # and make our slurm file 
+    txt_acc = '\n'.join(["#!/bin/bash -l",
+                                 "#SBATCH --account=blanca-casa\n",
+                                 "#SBATCH --partition=blanca-casa\n",
+                                 "#SBATCH --qos=blanca-casa\n"
+                                 "#SBATCH --time={:d}:00:00\n".format(wtime),
+                                 "#SBATCH --nodes=1",
+                                 "#SBATCH --ntasks=1",
+                                 "#SBATCH --job-name={}".format(jname),
+                                 "#SBATCH --array=0-{}".format(int(njobs-1)),
+                                 "#SBATCH --output={}/{}_{}_%A_%a.out".format(log_dir, jname, ts),
+                                 "#SBATCH --error={}/{}_{}_%A_%a.err".format(log_dir, jname, ts),
+                                 "",
+                                 'now=$(date +"%T")',
+                                 'echo "start time ... $now"',
+                                 'echo "Running task ID: ${SLURM_ARRAY_TASK_ID}"',
+                                 "",
+                                 'module purge',
+                                 'module load anaconda',
+                                 "source activate {}".format(env),
+                                 "",
+                                 "cd {}".format(code_dir),
+                                 _cmd,
+                                 'now=$(date +"%T")',
+                                 'echo "end time ... $now"',
+                                 ""])
+
+if process==0: ### first run
     njobs = 1000
     ids_split = np.array_split(ids, njobs)
     # save each chunk to a separate file
@@ -83,7 +123,7 @@ if process==0:
                                  'echo "end time ... $now"',
                                  ""])
 
-if process==1:
+if process==1: ### second run
     # this is added to adjust ids, for the last ~a few % of the sources
     ids_comp_ = glob.glob(outdir+"npz/*_spec_phisfh.h5")
     ids_comp = [float(a.split("/")[-1].split("_")[1]) for a in ids_comp_]
@@ -97,8 +137,10 @@ if process==1:
     # and make our slurm file 
     txt_acc = '\n'.join(["#!/bin/bash -l",
                                  "#SBATCH --account=blanca-casa\n",
-                                 "#SBATCH --partition=blanca\n",
-                                 "#SBATCH --qos=preemptable\n",
+#                                 "#SBATCH --partition=blanca\n",
+#                                 "#SBATCH --qos=preemptable\n",
+                                 "#SBATCH --partition=blanca-casa\n",
+                                 "#SBATCH --qos=blanca-casa\n"
                                  "#SBATCH --time={:d}:00:00\n".format(wtime),
                                  "#SBATCH --nodes=1",
                                  "#SBATCH --ntasks=1",
@@ -121,7 +163,7 @@ if process==1:
                                  'echo "end time ... $now"',
                                  ""])
 
-if process==2:
+if process==2: ### broken files, need ids of the broken file
     # this is added to re-run several sources, which is broken in either spec or perc. typically ~a few % of the sources
     ids = ids
     print(len(ids))
@@ -135,8 +177,10 @@ if process==2:
     # and make our slurm file 
     txt_acc = '\n'.join(["#!/bin/bash -l",
                                  "#SBATCH --account=blanca-casa\n",
-                                 "#SBATCH --partition=blanca\n",
-                                 "#SBATCH --qos=preemptable\n",
+#                                 "#SBATCH --partition=blanca\n",
+#                                 "#SBATCH --qos=preemptable\n",
+                                 "#SBATCH --partition=blanca-casa\n",
+                                 "#SBATCH --qos=blanca-casa\n"
                                  "#SBATCH --time={:d}:00:00\n".format(wtime),
                                  "#SBATCH --nodes=1",
                                  "#SBATCH --ntasks=1",
