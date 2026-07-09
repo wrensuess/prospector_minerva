0078119396fc2dba5298a4343f43ddacd14c5dd5
ikkimitsuhashi <ikki0913astr@gmail.com>
2026-07-09 15:55:42 -0600

    change submit_test to test oldjax

--- emulator.py
+++ emulator.py
@@ -16,12 +16,13 @@ class EmulatorBeta(ProspectorParams):
 
     def __init__(self, model_params, fp=multiemul_file, obs=None, param_order=None):
         super(EmulatorBeta, self).__init__(model_params, param_order=param_order)
-        print("emulator fp =", fp)
-        print("type self.dat =", type(self.dat))
-        print("self.dat =", self.dat)
 
         # Load all emulator data.
         self.dat = np.load(fp, allow_pickle=True).all()
+
+        print("emulator fp =", fp)
+        print("type self.dat =", type(self.dat))
+        print("self.dat =", self.dat)
 
         # Determine the indices of the filters in obs['filters']
         #self.sorter = np.array([self.dat["filter_redir"][f.name] for f in obs["filters"]], dtype=int)
