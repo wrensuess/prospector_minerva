1bd86dc3b64fac646eea1240e0fc716c561eec3f
ikkimitsuhashi <ikki0913astr@gmail.com>
2026-07-14 16:59:36 -0600

    change to test outlier with MIRI

--- save_all.sh
+++ save_all.sh
@@ -5,13 +5,14 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
-#SBATCH --time=24:00:00
+#SBATCH --time=1:00:00
 #SBATCH --mem=40G
 #SBATCH --output=./../log_chain/%x_%j.out
 #SBATCH --error=./../log_chain/%x_%j.err
 
 module purge
 module load slurm/blanca
+
 
 python save_sfh.py --dir_indiv="/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2_outliers_wMIRI/npz" --dir_collected="/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2_outliers_wMIRI" --n_workers=20
 
