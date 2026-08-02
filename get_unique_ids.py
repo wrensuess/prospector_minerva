5abd60df6a3fdc4e507695663461f13851deb217
ikkimitsuhashi <ikki0913astr@gmail.com>
2026-08-02 14:18:39 -0600

    check completion rate

--- get_unique_ids.py
+++ get_unique_ids.py
@@ -75,4 +75,5 @@ print("only fitting (=remaining):",len(only_fit_all))
 print("only fitted:",len(only_fitted_all))
 
 print(only_fit_all[:100])
-print(fitid1[:100])
+#print(fitid1[:100])
+np.savetxt(cat_dir+"refitid_MINERVA-COSMOS_n3.0_v1.0_ACS+WEBB_Kf444w_SUPER_CATALOG.txt",only_fit_all)
