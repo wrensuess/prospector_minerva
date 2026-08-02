e874420b5d36670bf420b4bbbcf682c10858026d
ikkimitsuhashi <ikki0913astr@gmail.com>
2026-08-02 14:15:00 -0600

    check completion rate

--- get_unique_ids.py
+++ get_unique_ids.py
@@ -74,4 +74,4 @@ print("overlap check:",len(common_all))
 print("only fitting (=remaining):",len(only_fit_all))
 print("only fitted:",len(only_fitted_all))
 
-print(only_fit_all[:,100])
+print(only_fit_all[:100])
