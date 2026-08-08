acd9fc6c1bb1ebde80692f837af678c19fc09599
ikkimitsuhashi <ikki0913astr@gmail.com>
2026-08-08 01:40:57 -0600

    postprocess check COSMOS

--- save_all.sh
+++ save_all.sh
@@ -16,15 +16,15 @@ module load slurm/blanca
 #"n3.0_m3.1_v1.2.1"
 #"n3.0_v1.2"
 
-python save_sfh.py --dir_indiv="/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2/npz" --dir_collected="/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2" --n_workers=20
+python save_sfh.py --dir_indiv="/scratch/alpine/ikmi3774/slurm/postprocess_COSMOS_n3.0_v1.0/npz" --dir_collected="/scratch/alpine/ikmi3774/slurm/postprocess_COSMOS_n3.0_v1.0" --n_workers=20
 
 ### this is not good due to time out, run save_chain.sh+combine_chain.py first
 #python save_chain.py --dir_indiv="/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2_goodpz_fagn1e5/npz" --dir_collected="/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2_goodpz_fagn1e5" --n_workers=20
 
-python save_spec.py --dir_indiv="/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2/npz" --dir_collected="/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2" --catalog_path="/projects/ikmi3774/minerva_sps_git/stellar_pop_catalog_bb/phot_catalog/MINERVA-COSMOS_n3.0_v1.0_ACS+WEBB_Kf444w_SUPER_CATALOG.fits" --n_workers=20
+python save_spec.py --dir_indiv="/scratch/alpine/ikmi3774/slurm/postprocess_COSMOS_n3.0_v1.0/npz" --dir_collected="/scratch/alpine/ikmi3774/slurm/postprocess_COSMOS_n3.0_v1.0" --catalog_path="/projects/ikmi3774/minerva_sps_git/stellar_pop_catalog_bb/phot_catalog/MINERVA-COSMOS_n3.0_v1.0_ACS+WEBB_Kf444w_SUPER_CATALOG.fits" --n_workers=20
 
-python save_perc.py --dir_indiv="/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2/npz" --dir_collected="/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2" --catalog_path="/projects/ikmi3774/minerva_sps_git/stellar_pop_catalog_bb/phot_catalog/MINERVA-COSMOS_n3.0_v1.0_ACS+WEBB_Kf444w_SUPER_CATALOG.fits" --n_workers=20
+python save_perc.py --dir_indiv="/scratch/alpine/ikmi3774/slurm/postprocess_COSMOS_n3.0_v1.0/npz" --dir_collected="/scratch/alpine/ikmi3774/slurm/postprocess_COSMOS_n3.0_v1.0" --catalog_path="/projects/ikmi3774/minerva_sps_git/stellar_pop_catalog_bb/phot_catalog/MINERVA-COSMOS_n3.0_v1.0_ACS+WEBB_Kf444w_SUPER_CATALOG.fits" --n_workers=20
 
-python save_final_catalog.py --dir_indiv="/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2/npz" --dir_collected="/scratch/alpine/ikmi3774/slurm/postprocess_UDS_n3.0_v1.2" --catalog_path="/projects/ikmi3774/minerva_sps_git/stellar_pop_catalog_bb/phot_catalog/MINERVA-COSMOS_n3.0_v1.0_ACS+WEBB_Kf444w_SUPER_CATALOG.fits"
+python save_final_catalog.py --dir_indiv="/scratch/alpine/ikmi3774/slurm/postprocess_COSMOS_n3.0_v1.0/npz" --dir_collected="/scratch/alpine/ikmi3774/slurm/postprocess_COSMOS_n3.0_v1.0" --catalog_path="/projects/ikmi3774/minerva_sps_git/stellar_pop_catalog_bb/phot_catalog/MINERVA-COSMOS_n3.0_v1.0_ACS+WEBB_Kf444w_SUPER_CATALOG.fits"
 
 
